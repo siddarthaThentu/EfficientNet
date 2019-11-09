38c36c267835855fbc484a110fdf95684ed1a51e
Siddartha Thentu <55814855+siddarthaThentu@users.noreply.github.com>
2019-11-08 17:41:23 -0800

    Add files via upload

--- untitled0.py
+++ untitled0.py
@@ -9,17 +9,19 @@ from tensorflow.keras.utils import plot_model
 from IPython.display import Image
 from keras.utils import to_categorical
 import matplotlib.pyplot as plt
+from skimage.transform import resize
+import numpy as np
+import cv2
+from keras import backend as K
 
 baseModel = EfficientNetB0(weights="imagenet")
 #print(baseModel.summary())
 
 dropout_rate = 0.2
 batch_size = 64
-width = 32
-height = 32
 epochs = 25
 
-baseModel = EfficientNetB0(weights="imagenet",include_top=False,input_shape=(height,width,3))
+baseModel = EfficientNetB0(weights="imagenet",include_top=False)
 #print(baseModel.summary())
 
 model = models.Sequential()
@@ -32,20 +34,28 @@ if dropout_rate > 0:
 model.add(layers.Dense(100,activation="softmax",name="fc_out"))
 
 #print(model.summary())
-baseModel.trainable = False
+#baseModel.trainable = False
 
 (x_train, y_train), (x_test, y_test) = cifar100.load_data(label_mode='fine')
 y_train = to_categorical(y_train,100)
 y_test = to_categorical(y_test,100)
+print(type(x_train))
+#x_train = resize(x_train,(40,40,3))
 
-#x_train = x_train.reshape(50000,225,225,3)
-#print(x_train.shape)
+img_rows = 227
+img_cols = 227
 
-NUM_TRAIN = len(x_train)
-NUM_TEST = len(x_test)
+if K.image_data_format() == 'th':
+        x_train = np.array([cv2.resize(img.transpose(1,2,0), (img_rows,img_cols)).transpose(2,0,1) for img in x_train[:len(x_train),:,:,:]])
+        x_test = np.array([cv2.resize(img.transpose(1,2,0), (img_rows,img_cols)).transpose(2,0,1) for img in x_test[:len(x_test),:,:,:]])
+else:
+        x_train = np.array([cv2.resize(img, (img_rows,img_cols)) for img in x_train[:len(x_train),:,:,:]])
+        x_test = np.array([cv2.resize(img, (img_rows,img_cols)) for img in x_test[:len(x_test),:,:,:]])
 
-train_datagen = ImageDataGenerator(
-    rescale=1.0 / 255
+print(x_train.shape)
+
+#train_datagen = ImageDataGenerator(
+#	  rescale=1.0 / 255
 #    rotation_range=40,
 #    width_shift_range=0.2,
 #    height_shift_range=0.2,
@@ -53,13 +63,13 @@ train_datagen = ImageDataGenerator(
 #    zoom_range=0.2,
 #    horizontal_flip=True,
 #    fill_mode="nearest"
-)
+#)
 
-train_datagen.fit(x_train)
+#train_datagen.fit(x_train)
                   
-validation_datagen = ImageDataGenerator(rescale=1.0 / 255)
+#validation_datagen = ImageDataGenerator(rescale=1.0 / 255)
 
-validation_datagen.fit(x_test)
+#validation_datagen.fit(x_test)
 
 model.compile(
     loss="categorical_crossentropy",
@@ -67,19 +77,21 @@ model.compile(
     metrics=["acc"]
 )
 
-hist =  model.fit_generator(
-           train_datagen.flow(x_train, y_train, batch_size=batch_size),
-           steps_per_epoch=len(x_train) // batch_size, 
-           epochs=epochs,
-           validation_data=validation_datagen.flow(x_test,y_test,batch_size=batch_size),
-           validation_steps=len(x_test) // batch_size,
-           verbose=1)
-           #use_multiprocessing=True,
-           #orkers=4)
+#hist =  model.fit_generator(
+ #          train_datagen.flow(x_train, y_train, batch_size=batch_size),
+  #         steps_per_epoch=len(x_train) // batch_size, 
+  #         epochs=epochs,
+  #         validation_data=validation_datagen.flow(x_test,y_test,batch_size=batch_size),
+  #         validation_steps=len(x_test) // batch_size,
+  #         verbose=1)
+#           #use_multiprocessing=True,
+#           #orkers=4)
+#	
 
-#hist = model.fit(x_train, y_train, batch_size=32, epochs=50,validation_split=0.2)
-plot_model(baseModel,to_file="baseModel.png",show_shapes=True)
-Image(filename="baseModel.png")
+hist = model.fit(x_train, y_train, batch_size=32, epochs=50,validation_split=0.2)
+
+#plot_model(baseModel,to_file="baseModel.png",show_shapes=True)
+#Image(filename="baseModel.png")
 
 model.save("efficientSample.h5")
 
@@ -101,21 +113,21 @@ plt.show()
 
 #model.evaluate(x_test,y_test)[1]
 #baseModel.trainable = True
-
-#set_trainable = False
-#for layer in baseModel.layers:
-#    if layer.name == 'multiply_16':
-#        set_trainable = True
-#    if set_trainable:
-#        layer.trainable = True
-#    else:
-#        layer.trainable = False
 #
-
-
-
-
-
+##set_trainable = False
+##for layer in baseModel.layers:
+##    if layer.name == 'multiply_16':
+##        set_trainable = True
+##    if set_trainable:
+##        layer.trainable = True
+##    else:
+##        layer.trainable = False
+##
+#
+#
+#
+#
+#
 
 
 
